b8ab4c4dd5ea84baf077287adcd0cc1c40116c85
hellomohitsangwan <hellomohitsangwan@gmail.com>
2023-11-23 17:30:51 +0530

    flipkart scrqpper v-1

--- server/controllers/scrapecontroller.js
+++ server/controllers/scrapecontroller.js
@@ -3,13 +3,13 @@ import * as cheerio from "cheerio"
 import { scrapeAmazon, scrapeFlipkart } from "../utils/scrapper.js";
 
 export const scrapeWeb = asyncHandler(async (req, res) => {
-    let product = "apple"
+    let product = "apple iphone"
     let n = 8;
 
     const flipkartRes = await scrapeFlipkart(product, n);
 
-    console.log(awazonRes.length);
-    res.json({res: awazonRes})
+    // console.log(awazonRes.length);
+    res.json({res: flipkartRes})
 
 });
 
--- server/utils/scrapper.js
+++ server/utils/scrapper.js
@@ -68,51 +68,35 @@ export const scrapeFlipkart = async (product, no_of_products) => {
         headers,
     });
     const text = await resp.text();
-    console.log(text);
 
     const $ = cheerio.load(text);
-    const divsWithClassX = $(".s-widget-spacing-small");
+    const divsWithClassX = $("._13oc-S");
 
     const resultArray = [];
     divsWithClassX.each((index, element) => {
-        if (resultArray.length >= no_of_products) return resultArray;
-        const ele = $(element).html();
-        const $element = cheerio.load(ele);
-
-        // Extract name
-        const name = $element("h2 a span").text().trim();
-
-        // Extract prices
-        const priceContainer = $element(".a-price");
-        const discountedPrice = priceContainer.find(".a-offscreen").text().trim();
-        const originalPrice =
-            priceContainer.find(".a-price-symbol + .a-price-whole").text().trim() ||
-            discountedPrice;
-
-        // Extract rating
-        const rating = $element(".a-icon-star-small .a-icon-alt").text().trim();
-
-        // Extract URL
-        const url = $element("h2 a").attr("href");
-        const productUrl = `https://www.amazon.in${url}`;
-
-        // Extract total review count
-        const totalReviewsContainer = $element(
-            ".a-section .a-row.a-size-small span.a-size-base"
-        );
-        const totalReviews = totalReviewsContainer.text().trim();
-
+        if (resultArray.length >= no_of_products) return;
+        const title = $(element).find('._4rR01T').text().trim();
+        const rating = $(element).find('span.f3A4_V .gUuXy- .hGSR34').text().trim();
+        const numRatings = $(element).find('span._2_R_DZ span').text().trim();
+        const features = [];
+        $(element).find('div.fMghEO ul._1xgFaf li').each((_, featureElement) => {
+            features.push($(featureElement).text().trim());
+        });
+        const price = $(element).find('div._30jeq3').text().trim();
+        const originalPrice = $(element).find('div._3I9_wc').text().trim();
+        const discount = $(element).find('div._3Ay6Sb span').text().trim();
 
         resultArray.push({
-            name,
-            discountedPrice,
-            originalPrice,
+            title,
             rating,
-            productUrl,
-            totalReviews,
+            numRatings,
+            features,
+            price,
+            originalPrice,
+            discount,
         });
     });
 
-    return resultArray
+    return resultArray;
 };
 
