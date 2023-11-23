f89b56fb89b014540e226a8c8ceb38cd82651f76
hellomohitsangwan <hellomohitsangwan@gmail.com>
2023-11-24 02:47:29 +0530

    upload route and bug fix

--- server/server.js
+++ server/server.js
@@ -2,12 +2,20 @@ import path from "path";
 import express from "express";
 import scrapeRoutes from "./routes/scrapeRoutes.js";
 import userRoutes from "./routes/userRoutes.js";
+import uploadRoutes from "./routes/uploadRoutes.js";
+
+import cloudinary from "cloudinary";
+import morgan from "morgan";
+import fileUpload from "express-fileupload";
 
 const app = express();
 app.use(express.json());
 
+const __dirname = path.resolve();
+
 app.use("/api/scrape", scrapeRoutes)
 app.use("/api/users", userRoutes);
+app.use("/uploads", express.static(path.join(__dirname, "/uploads")));
 
 
 const PORT = 8000;
