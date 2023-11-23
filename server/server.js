2e25e60fedab9d108815e0bac71037b3d5ec155d
hellomohitsangwan <hellomohitsangwan@gmail.com>
2023-11-23 15:34:21 +0530

    amazon scrapper v-1

--- server/server.js
+++ server/server.js
@@ -1,8 +1,12 @@
-const path = require("path");
-const express = require("express");
+import path from "path";
+import express from "express";
+import scrapeRoutes from "./routes/scrapeRoutes.js";
+
 
 const app = express();
 app.use(express.json());
+
+app.use("/api/scrape", scrapeRoutes)
 
 
 const PORT = 8000;
