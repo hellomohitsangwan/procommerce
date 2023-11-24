a30b41bb71bd89b206fe9a4d9d116c7a6ba01295
hellomohitsangwan <hellomohitsangwan@gmail.com>
2023-11-24 16:45:36 +0530

    Dashboard screen stru

--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,7 @@ import Register from "./screens/RegisterScreen";
 import HomeScreen from "./screens/HomeScreen";
 import Login from "./screens/LoginScreen";
 import Header from "./components/Header";
+import DashboardScreen from "./screens/DashboardScreen";
 
 function App() {
   return (
@@ -14,6 +15,7 @@ function App() {
         
         <Route path="/register" component={Register} />
         <Route path="/login" component={Login} />
+        <Route path="/dashboard" component={DashboardScreen}/>
       </main>
     </Router>
   );
---
+++ client/src/screens/DashboardScreen.js
@@ -0,0 +1,115 @@
+import React from "react";
+import "./Screen.css";
+import { Link } from "react-router-dom";
+import dashArrow from "../assets/dashArrow.svg";
+import dashFarmer from "../assets/dashboardBackground.svg";
+import { useSelector } from "react-redux";
+
+const DashboardScreen = () => {
+  const userLogin = useSelector((state) => state.userLogin);
+  const { userInfo } = userLogin;
+  return (
+    <>
+      {userInfo && userInfo.isAdmin && (
+        <div>
+          <div className="background">
+            <img src={dashFarmer} alt="" />
+          </div>
+          <div className="dashboard">
+            <div className="amount-dashboard">
+              <div className="amount-rect">
+                <div className="rect-details">
+                  <div className="gap"></div>
+                  <div className="heading">
+                    <p className="amt-title">Admin's Dashboard</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="row2">
+              <div className="products-dashboard">
+                <div className="products-rect">
+                  <div className="rect-details">
+                    <div className="gap"></div>
+                    <div className="heading">
+                      <p>Products</p>
+                    </div>
+                    <div className="dashArrow">
+                      <Link to={"/admin/productlist"}>
+                        <button className="dashBtn">
+                          <img src={dashArrow} alt="" />
+                        </button>
+                      </Link>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div className="orders-dashboard">
+                <div className="orders-rect">
+                  <div className="gap"></div>
+                  <div className="rect-details">
+                    <div className="heading">
+                      <p>Orders</p>
+                    </div>
+                    <div className="dashArrow">
+                      <Link to={"/admin/orderlist"}>
+                        <button className="dashBtn">
+                          <img src={dashArrow} alt="" />
+                        </button>
+                      </Link>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="row3">
+              <div className="users-dashboard">
+                <div className="user-rect">
+                  <div className="gap"></div>
+                  <div className="rect-details">
+                    <div className="heading">
+                      <p>Recom</p>
+                    </div>
+                    <div className="dashArrow1 ">
+                      <Link to={"/recommendations"}>
+                        <button className="dashBtn3">
+                          <img src={dashArrow} alt="" />
+                        </button>
+                      </Link>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div className="reviews-dashboard">
+                <div className="reviews-rect">
+                  <div className="gap"></div>
+                  <div className="rect-details">
+                    <div className="heading">
+                      <p>Reviews</p>
+                    </div>
+                    <div className="dashArrow">
+                      <button className="dashBtn2">
+                        <img src={dashArrow} alt="" />
+                      </button>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div className="stock-dashboard">
+                <div className="stock-rect">
+                  <div className="gap"></div>
+                  <div className="rect-details">
+                    <div className="heading">
+                      <p>Out of Stock</p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      )}
+    </>
+  );
+};
+export default DashboardScreen;
