c83dc4472993ea3c0a47be2bbbdb32be9972feb1
hellomohitsangwan <hellomohitsangwan@gmail.com>
2023-11-24 23:54:55 +0530

    cart constnts and actions

--- client/src/App.js
+++ client/src/App.js
@@ -8,6 +8,7 @@ import DashboardScreen from "./screens/DashboardScreen";
 import ProductListScreen from "./screens/ProductlistScreen";
 import NewProductScreen from "./screens/NewProductScreen";
 import ProductScreen from "./screens/ProductScreen";
+import CartScreen from "./screens/CartScreen";
 
 function App() {
   return (
@@ -22,6 +23,7 @@ function App() {
         <Route path="/dashboard" component={DashboardScreen} />
         <Route path="/admin/productlist" component={ProductListScreen} />
         <Route path="/admin/product/new" component={NewProductScreen} />
+        <Route path="/cart/:id?" component={CartScreen} />
       </main>
     </Router>
   );
---
+++ client/src/actions/cartActions.js
@@ -0,0 +1,47 @@
+import axios from "axios";
+import {
+  CART_ADD_ITEM,
+  CART_DELETE_ITEM,
+  CART_SAVE_SHIPPING_ADDRESS,
+  CART_SAVE_PAYMENT_METHOD,
+} from "../constants/cartConstants";
+
+export const addToCart = (id, qty) => async (dispatch, getState) => {
+  //getState is just toState of whatever reducer we want preset in our state
+  const { data } = await axios.get(`/api/products/${id}`);
+  dispatch({
+    type: CART_ADD_ITEM,
+    payload: {
+      product: data.product._id,
+      name: data.product.name,
+      images: data.product.images,
+      price: data.product.price,
+      countInStock: data.product.countInStock,
+      qty,
+    },
+  });
+  localStorage.setItem("cartItems", JSON.stringify(getState().cart.cartItems));
+};
+export const removeFromCart = (id) => async (dispatch, getState) => {
+  dispatch({
+    type: CART_DELETE_ITEM,
+    payload: id,
+  });
+  localStorage.setItem("cartItems", JSON.stringify(getState().cart.cartItems));
+};
+
+export const saveShippingAddress = (data) => async (dispatch, getState) => {
+  dispatch({
+    type: CART_SAVE_SHIPPING_ADDRESS,
+    payload: data,
+  });
+  localStorage.setItem("shippingAddress", JSON.stringify(data));
+};
+
+export const savePaymentMethod = (data) => async (dispatch, getState) => {
+  dispatch({
+    type: CART_SAVE_PAYMENT_METHOD,
+    payload: data,
+  });
+  localStorage.setItem("paymentMethod", JSON.stringify(data));
+};
