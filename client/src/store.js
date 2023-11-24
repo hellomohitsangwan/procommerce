c7d0e72623583c0ef684f9f9945cde17a514c178
hellomohitsangwan <hellomohitsangwan@gmail.com>
2023-11-24 14:31:17 +0530

    redux store setup

---
+++ client/src/store.js
@@ -0,0 +1,21 @@
+import { createStore, applyMiddleware, combineReducers } from "redux";
+import { composeWithDevTools } from "redux-devtools-extension";
+import thunk from "redux-thunk";
+
+
+const reducer = combineReducers({
+});
+
+
+const initialState = {
+};
+
+const middleware = [thunk];
+
+const store = createStore(
+  reducer,
+  initialState,
+  composeWithDevTools(applyMiddleware(...middleware))
+);
+
+export default store;
