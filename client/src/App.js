af6bf35084d419c0a38aec5faf3abaa9a3da1801
hellomohitsangwan <hellomohitsangwan@gmail.com>
2023-11-24 14:54:14 +0530

    react router setup

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,15 @@
-import './App.css';
+import "./App.css";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      Home 
-    </div>
+    <Router>
+      <main>
+        <Route></Route>
+      </main>
 
+      <div>Home</div>
+    </Router>
   );
 }
 
