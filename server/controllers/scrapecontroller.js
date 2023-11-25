1eb443d42dc04ec318b94e88df588a888130bb02
hellomohitsangwan <hellomohitsangwan@gmail.com>
2023-11-25 21:26:24 +0530

    mongo db scrapeProduct data save

--- server/controllers/scrapecontroller.js
+++ server/controllers/scrapecontroller.js
@@ -6,136 +6,200 @@ import {
   scrapeShopclues,
   scrapeSnapdeal,
 } from "../utils/scrapper.js";
+import ScrapedProduct from "../models/scrapedProductModel.js";
+
+// export const scrapeWeb = asyncHandler(async (req, res) => {
+
+//   try {
+//     const { searchTerm, filter, topN, comparisonWebsites } = req.body;
+
+//     const websiteScrapingFunctions = {
+//         amazon: scrapeAmazon,
+//         flipkart: scrapeFlipkart,
+//         shopclues: scrapeShopclues,
+//         snapdeal: scrapeSnapdeal,
+//         nykaa: scrapeNykaa,
+//     };
+
+//     // Fetch data from scraping functions for selected websites
+//     const websitePromises = comparisonWebsites.map(async (website) => {
+//         if (websiteScrapingFunctions[website]) {
+//             const websiteData = await websiteScrapingFunctions[website](searchTerm, topN);
+//             // Add a 'website' field to each item in the data
+//             return websiteData.map(item => ({ ...item, website }));
+//         }
+//         return [];
+//     });
+
+//     // Wait for all promises to resolve
+//     const websiteDataArrays = await Promise.all(websitePromises);
+
+//     // Combine data from selected websites
+//     const allData = websiteDataArrays.flat();
+
+//     // Apply filtering if specified
+//     let filteredData = allData;
+//     if (filter === 'highestPrice') {
+//         filteredData = allData.sort((a, b) => b.discountedPrice - a.discountedPrice);
+//     } else if (filter === 'lowestPrice') {
+//         filteredData = allData.sort((a, b) => a.discountedPrice - b.discountedPrice);
+//     } else if (filter === 'highestRating') {
+//         filteredData = allData.sort((a, b) => b.rating - a.rating);
+//     }
+
+//     // Take the top N results
+//     const topResults = filteredData.slice(0, topN);
+
+//     // Prepare response array
+//     const responseArray = topResults.map((item) => ({
+//         url: item.productUrl,
+//         title: item.name,
+//         reviewCount: item.totalReviews,
+//         rating: item.rating,
+//         currentPrice: item.discountedPrice,
+//         website: item.website, // Add the 'website' field
+//         // Add more fields as needed
+//     }));
+
+//     res.json(responseArray);
+// } catch (error) {
+//     console.error(error);
+//     res.status(500).json({ error: 'Internal Server Error' });
+// }
+// });
+
+
+
+// export const scrapeWeb = asyncHandler(async (req, res) => {
+//   try {
+//       const { searchTerm, filter, topN, comparisonWebsites } = req.body;
+
+//       const websiteScrapingFunctions = {
+//           amazon: scrapeAmazon,
+//           flipkart: scrapeFlipkart,
+//           shopclues: scrapeShopclues,
+//           snapdeal: scrapeSnapdeal,
+//           nykaa: scrapeNykaa,
+//       };
+
+//       const websitePromises = comparisonWebsites.map(async (website) => {
+//           if (websiteScrapingFunctions[website]) {
+//               const websiteData = await websiteScrapingFunctions[website](searchTerm, topN);
+
+//               // Filter out items without a name
+//               const filteredData = websiteData.filter(item => item.name);
+
+//               return filteredData.map(item => ({ ...item, website }));
+//           }
+//           return [];
+//       });
+
+//       // Wait for all promises to resolve
+//       const websiteDataArrays = await Promise.all(websitePromises);
+
+//       const allData = websiteDataArrays.flat();
+
+//       // Apply filtering if specified
+//       let filteredData = allData;
+//       if (filter === 'highestPrice') {
+//           filteredData = allData.sort((a, b) => b.discountedPrice - a.discountedPrice);
+//       } else if (filter === 'lowestPrice') {
+//           filteredData = allData.sort((a, b) => a.discountedPrice - b.discountedPrice);
+//       } else if (filter === 'highestRating') {
+//           filteredData = allData.sort((a, b) => b.rating - a.rating);
+//       }
+
+//       // Take the top N results
+//       const topResults = filteredData.slice(0, topN);
+
+     
+//       const responseArray = topResults.map((item) => ({
+//           url: item.productUrl,
+//           title: item.name,
+//           reviewCount: item.totalReviews,
+//           rating: item.rating,
+//           currentPrice: item.discountedPrice,
+//           website: item.website, 
+          
+//       }));
+
+//       res.json(responseArray);
+//   } catch (error) {
+//       console.error(error);
+//       res.status(500).json({ error: 'Internal Server Error' });
+//   }
+// });
+
+
+
+
+
+
+
+
 
 export const scrapeWeb = asyncHandler(async (req, res) => {
-  //   let product = "iphone";
-  //   let n = 8;
-
-  //   const flipkartRes = await scrapeFlipkart(product, n);
-  //   const amazonRes = await scrapeAmazon(product, n);
-  //   const nykaaRes = await scrapeNykaa(product, n);
-  //   const ShopcluestRes = await scrapeShopclues(product, n);
-  //   const snapdealRes = await scrapeSnapdeal(product, n);
-
-  //   // console.log(awazonRes.length);
-  //   res.json({ res: flipkartRes });
-
   try {
-    const { searchTerm, filter, topN, comparisonWebsites } = req.body;
+      const { searchTerm, filter, topN, comparisonWebsites } = req.body;
 
-    const websiteScrapingFunctions = {
-        amazon: scrapeAmazon,
-        flipkart: scrapeFlipkart,
-        shopclues: scrapeShopclues,
-        snapdeal: scrapeSnapdeal,
-        nykaa: scrapeNykaa,
-    };
+      const websiteScrapingFunctions = {
+          amazon: scrapeAmazon,
+          flipkart: scrapeFlipkart,
+          shopclues: scrapeShopclues,
+          snapdeal: scrapeSnapdeal,
+          nykaa: scrapeNykaa,
+      };
 
-    // Fetch data from scraping functions for selected websites
-    const websitePromises = comparisonWebsites.map(async (website) => {
-        if (websiteScrapingFunctions[website]) {
-            const websiteData = await websiteScrapingFunctions[website](searchTerm, topN);
-            // Add a 'website' field to each item in the data
-            return websiteData.map(item => ({ ...item, website }));
-        }
-        return [];
-    });
+      // Fetch data from scraping functions for selected websites
+      const websitePromises = comparisonWebsites.map(async (website) => {
+          if (websiteScrapingFunctions[website]) {
+              const websiteData = await websiteScrapingFunctions[website](searchTerm, topN);
 
-    // Wait for all promises to resolve
-    const websiteDataArrays = await Promise.all(websitePromises);
+              // Filter out items without a name
+              const filteredData = websiteData.filter(item => item.name);
 
-    // Combine data from selected websites
-    const allData = websiteDataArrays.flat();
+              // Save data to MongoDB
+              await ScrapedProduct.insertMany(filteredData);
 
-    // Apply filtering if specified
-    let filteredData = allData;
-    if (filter === 'highestPrice') {
-        filteredData = allData.sort((a, b) => b.discountedPrice - a.discountedPrice);
-    } else if (filter === 'lowestPrice') {
-        filteredData = allData.sort((a, b) => a.discountedPrice - b.discountedPrice);
-    } else if (filter === 'highestRating') {
-        filteredData = allData.sort((a, b) => b.rating - a.rating);
-    }
+              // Add a 'website' field to each item in the data
+              return filteredData.map(item => ({ ...item, website }));
+          }
+          return [];
+      });
 
-    // Take the top N results
-    const topResults = filteredData.slice(0, topN);
+      // Wait for all promises to resolve
+      const websiteDataArrays = await Promise.all(websitePromises);
 
-    // Prepare response array
-    const responseArray = topResults.map((item) => ({
-        url: item.productUrl,
-        title: item.name,
-        reviewCount: item.totalReviews,
-        rating: item.rating,
-        currentPrice: item.discountedPrice,
-        website: item.website, // Add the 'website' field
-        // Add more fields as needed
-    }));
+      // Combine data from selected websites
+      const allData = websiteDataArrays.flat();
 
-    res.json(responseArray);
-} catch (error) {
-    console.error(error);
-    res.status(500).json({ error: 'Internal Server Error' });
-}
+      // Apply filtering if specified
+      let filteredData = allData;
+      if (filter === 'highestPrice') {
+          filteredData = allData.sort((a, b) => b.discountedPrice - a.discountedPrice);
+      } else if (filter === 'lowestPrice') {
+          filteredData = allData.sort((a, b) => a.discountedPrice - b.discountedPrice);
+      } else if (filter === 'highestRating') {
+          filteredData = allData.sort((a, b) => b.rating - a.rating);
+      }
+
+      // Take the top N results
+      const topResults = filteredData.slice(0, topN);
+
+      // Prepare response array
+      const responseArray = topResults.map((item) => ({
+          url: item.productUrl,
+          title: item.name,
+          reviewCount: item.totalReviews,
+          rating: item.rating,
+          currentPrice: item.discountedPrice,
+          website: item.website, // Add the 'website' field
+          // Add more fields as needed
+      }));
+
+      res.json(responseArray);
+  } catch (error) {
+      console.error(error);
+      res.status(500).json({ error: 'Internal Server Error' });
+  }
 });
-
-// {
-//     name,
-//     discountedPrice,
-//     originalPrice,
-//     rating,
-//     productUrl,
-//     totalReviews,
-// }
-
-const Amzon = {
-  name: "Apple iPhone 13 (128GB) - Starlight",
-  discountedPrice: 52499,
-  originalPrice: 52499,
-  rating: 4.5,
-  productUrl:
-    "https://www.amazon.in/sspa/click?ie=UTF8&spc=MTo1MTQ0ODg4MTE1MzQwNjkyOjE3MDA4NjE0MTE6c3BfYXRmOjIwMDMwOTI2MTUxMDk4OjowOjo&url=%2FApple-iPhone-13-128GB-Starlight%2Fdp%2FB09G9D8KRQ%2Fref%3Dsr_1_1_sspa%3Fkeywords%3Diphone%26qid%3D1700861411%26sr%3D8-1-spons%26sp_csd%3Dd2lkZ2V0TmFtZT1zcF9hdGY%26psc%3D1",
-  totalReviews: 20636,
-  discount: 0,
-};
-
-const flpkt = {
-  name: "APPLE iPhone 14 (Blue, 128 GB)",
-  discountedPrice: 60999,
-  originalPrice: 69900,
-  rating: 4.6,
-  totalReviews: 2358949062,
-  discount: 12,
-};
-
-const shopclues = {
-  name: "(Refurbished) Apple Iphone 7 2 Gb RAM 32Gb ROM Single S",
-  imageSrc:
-    "https://cdn.shopclues.com/images1/thumbnails/117328/200/200/153427531-117328593-1691669515.jpg",
-  productUrl:
-    "//www.shopclues.com/refurbished-apple-iphone-7-2-gb-ram-32gb-rom-single-sim-smartphone-superb-condition-like-newrefur-210-153427531.html",
-  discountedPrice: 35000,
-  discount: 60,
-  refurbishedBadge: "Refurbished",
-};
-
-const snapdeal = {
-  name: "NBOX - Black Silicon Plain Cases Compatible For Apple iPhone 11 ( Pack of 1 )",
-  imageSrc:
-    "https://n4.sdlcdn.com/imgs/k/m/n/230X258_sharpened/NBOX-Black-Silicon-Plain-Cases-SDL540321070-1-84875.jpg",
-  originalPrice: 1299,
-  discountedPrice: 399,
-  discount: 69,
-  rating: 4,
-  numRatings: 6,
-};
-
-const nykaa = {
-  name: "Maybelline New York Color Sensational Creamy Matte Lipstick",
-  productUrl:
-    "https://www.nykaa.com/maybelline-new-york-color-sensational-creamy-matte-lipsticks/p/353926?productId=353926&pps=1",
-  originalPrice: 329,
-  discountedPrice: 224,
-  discount: 32,
-  totalReviews: 110699,
-  rating: 4,
-};
